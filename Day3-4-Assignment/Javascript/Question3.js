4f0a13db8be739ce0714e220a9fc18a9476615ed
thecodertech <66317270+thecodertech@users.noreply.github.com>
2020-09-11 17:57:42 +0530

    Update Question3.js

--- Day3-4-Assignment/Javascript/Question3.js
+++ Day3-4-Assignment/Javascript/Question3.js
@@ -1,6 +1,31 @@
-let ObjectsArray[
+let ObjectsArray=[
     {
         name: "Ramesh",
         country: "India",
+        age: 24,
+        hobbies: ["painting","travelling","singing"],
+    },
+    {
+        name: "edison",
+        country: "america",
+        age: 36,
+        hobbies: ["swimming","travelling","singing"],
+    },
+    {
+        name: "ellie",
+        country: "Russia",
+        age:29,
+        hobbies: ["painting","singing"],
     },
 ];
+for(let i = 0; i < ObjectsArray.length; i++)
+{
+    console.log("name is "+ObjectsArray[i].name);
+    console.log("country is "+ObjectsArray[i].country);
+    console.log("age is "+ObjectsArray[i].age);
+    for(let j = 0; j<ObjectsArray[i].hobbies.length; j++)
+    {
+        console.log((j+1)+" hobbies are "+ObjectsArray[i].hobbies[j]);
+    }
+    console.log(" ");
+}
